94b178c67a814b3e030ebdf1eea4fb1f97aef280
John MacGregor <jmacgregor@skyllc.com>
2022-04-18 08:09:01 -0400

    Rename tests

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -7,25 +7,25 @@ internal class FizzBuzzTest {
     private val subject: FizzBuzz = FizzBuzz()
 
     @Test
-    fun fizzBuzz_returns_int() {
+    fun `Function fizzBuzz returns int when standard`() {
         val testArg = 1
         assertEquals(testArg.toString(), subject.fizzBuzz(testArg))
     }
 
     @Test
-    fun fizzBuzz_returns_fizz() {
+    fun `Function fizzBuzz returns Fizz for 3`() {
         val testArg = 3
         assertEquals("Fizz", subject.fizzBuzz(testArg))
     }
 
     @Test
-    fun fizzBuzz_returns_buzz() {
+    fun `Function fizzBuzz returns Buzz for 5`() {
         val testArg = 5
         assertEquals("Buzz", subject.fizzBuzz(testArg))
     }
 
     @Test
-    fun fizzBuzz_returns_fizzbuzz() {
+    fun `Function fizzBuzz returns FizzBuzz for 15`() {
         val testArg = 15
         assertEquals("FizzBuzz", subject.fizzBuzz(testArg))
     }
